82649b78055bc755c8470f3de522a42271288a92
Praise Tompane <praisetompane@icloud.com>
2025-09-11 10:28:33 +0200

    fix: selection sort implementation

--- 0_theory_of_computation/4_design_and_analysis_of_algorithms/0_data_structures_design_and_analysis/1_sorting_and_searching/sorting/test_selection_sort.py
+++ 0_theory_of_computation/4_design_and_analysis_of_algorithms/0_data_structures_design_and_analysis/1_sorting_and_searching/sorting/test_selection_sort.py
@@ -1,8 +1,22 @@
-from selection_sort import sort
+from selection_sort.impl.sort import sort, sort_explicit, SORT_DIRECTION
 
 
-def test_selection_sort():
+def test_sort():
     assert sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
     assert sort([4, 2, 3, 1, 5]) == [1, 2, 3, 4, 5]
     assert sort([]) == []
     assert sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+
+
+def test_bubble_sort_ascending():
+    assert sort_explicit([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+    assert sort_explicit([4, 2, 3, 1, 5]) == [1, 2, 3, 4, 5]
+    assert sort_explicit([]) == []
+    assert sort_explicit([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+
+
+def test_bubble_sort_descending():
+    assert sort_explicit([3, 2, 1, 4, 5], SORT_DIRECTION.DESC) == [5, 4, 3, 2, 1]
+    assert sort_explicit([5, 4, 3, 2, 1], SORT_DIRECTION.DESC) == [5, 4, 3, 2, 1]
+    assert sort_explicit([]) == []
+    assert sort_explicit([1, 2, 3, 4, 5], SORT_DIRECTION.DESC) == [5, 4, 3, 2, 1]
