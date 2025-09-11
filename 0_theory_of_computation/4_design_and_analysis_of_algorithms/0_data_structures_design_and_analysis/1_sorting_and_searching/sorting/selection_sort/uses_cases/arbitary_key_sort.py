5c8897757f5913238f17c3f6f41743544dbc967e
Praise Tompane <praisetompane@icloud.com>
2025-09-11 14:46:39 +0200

    fix: order by arbitary number of keys

--- 0_theory_of_computation/4_design_and_analysis_of_algorithms/0_data_structures_design_and_analysis/1_sorting_and_searching/sorting/selection_sort/uses_cases/arbitary_key_sort.py
+++ 0_theory_of_computation/4_design_and_analysis_of_algorithms/0_data_structures_design_and_analysis/1_sorting_and_searching/sorting/selection_sort/uses_cases/arbitary_key_sort.py
@@ -1,14 +1,11 @@
 from typing import List, Any
 
 
-def _sort(values: List[Any], key: str, previous_key: str):
+def _sort(values: List[Any], key: str):
     for target_sort_index in range(0, len(values)):  # O(𝑁)
         minimum_value_index = target_sort_index
-        for j in range(target_sort_index + 1, len(values)):  # O(𝑁)
-            if (
-                values[minimum_value_index][key] > values[j][key]
-                and values[minimum_value_index][previous_key] >= values[j][previous_key]
-            ):  # O(1)
+        for j in range(target_sort_index, len(values)):  # O(𝑁)
+            if values[minimum_value_index][key] > values[j][key]:  # O(1)
                 minimum_value_index = j
 
         if minimum_value_index != target_sort_index:
@@ -21,9 +18,7 @@ def _sort(values: List[Any], key: str, previous_key: str):
 
 def sort(values: List[Any], keys: List[str]):
     sorted_values = values
-    previous_key = keys[0]
-    for k in keys:
-        sorted_values = _sort(sorted_values, k, previous_key)
-        previous_key = k
+    for k in reversed(keys):
+        sorted_values = _sort(sorted_values, k)
 
     return sorted_values
