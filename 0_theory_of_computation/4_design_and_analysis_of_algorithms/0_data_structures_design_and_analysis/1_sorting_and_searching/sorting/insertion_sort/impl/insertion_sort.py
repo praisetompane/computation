c99430ec21dc16da6ad134b472166eb25f54525f
Praise Tompane <praisetompane@icloud.com>
2025-09-16 19:39:09 +0200

    fix: optimization: halt shuffling the 'value_to_insert' to its correct place in the sorted list once it is in place(i.e. all values to its left are less than it).

--- 0_theory_of_computation/4_design_and_analysis_of_algorithms/0_data_structures_design_and_analysis/1_sorting_and_searching/sorting/insertion_sort/impl/insertion_sort.py
+++ 0_theory_of_computation/4_design_and_analysis_of_algorithms/0_data_structures_design_and_analysis/1_sorting_and_searching/sorting/insertion_sort/impl/insertion_sort.py
@@ -4,10 +4,9 @@ from typing import List, Any
 def sort(values: List[Any]):
     for idx, value_to_insert in enumerate(values):
         i = idx - 1
-        while i >= 0:
-            if value_to_insert < values[i]:
-                temp = values[i]
-                values[i] = value_to_insert
-                values[i + 1] = temp
+        while i >= 0 and value_to_insert < values[i]:
+            temp = values[i]
+            values[i] = value_to_insert
+            values[i + 1] = temp
             i -= 1
     return values
